38e92d7d36cfab63e624f2556e04c8fbb9458b66
Shirin-Yan <shirin-yan@ShirinMBP.local>
2024-08-01 09:28:49 +0500

    Project created

---
+++ AffirmationsApp/AffirmationsApp/AffirmationsAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AffirmationsAppApp.swift
+//  AffirmationsApp
+//
+//  Created by Shirin-Yan on 31.07.2024.
+//
+
+import SwiftUI
+
+@main
+struct AffirmationsAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
